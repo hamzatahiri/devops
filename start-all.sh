228884a0e2541148199a46cf833c702b94afe1ed
htahiri <hamzatahiri@outlook.com>
2019-06-26 05:00:36 -0400

    am

--- start-all.sh
+++ start-all.sh
@@ -4,4 +4,4 @@
 ./ds/start.sh > /dev/null 2>&1 &
 ./ad/start.sh  > /dev/null 2>&1 &
 ./idm/start.sh  > /dev/null 2>&1 &
-
+./am/start.sh > /dev/null 2>&1 &
--- stop-all.sh
+++ stop-all.sh
@@ -4,4 +4,4 @@
 ./ds/stop.sh > /dev/null 2>&1 &
 ./ad/stop.sh  > /dev/null 2>&1 &
 ./idm/stop.sh  > /dev/null 2>&1 &
-
+./am/stop.sh > /dev/null 2>&1 &
